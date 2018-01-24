b36bf571e2df6393d654cdc5692d40b78ca7cd9f
sky <gksmfzz1@gmail.com>
2018-01-24 18:29:31 +0900

    2018-01-24

--- SungJukV4.py
+++ SungJukV4.py
@@ -1,10 +1,16 @@
-class SungJuk:
-    type = '성적처리 프로그램'
-    name = ' '
-    kor = 0
-    eng = 0
-    mat = 0
+#클래스를 이용해서 성적처리 프로그램 작성
 
+
+
+
+
+
+
+
+
+#print('이름=%s 국어점수=%d 영어점수=%d 수학점수=%d 총점=%d 평균점수=%.1f 학점=%s' % (sj.name, sj.kor, sj.eng, sj.mat, sj.getTotal(), sj.getAverage(),sj.getGrade()))
+
+class SungJukVO:
 
     def __init__(self,name,kor,eng,mat):
         self.name = name
@@ -12,27 +18,30 @@ class SungJuk:
         self.eng = eng
         self.mat = mat
 
-    def getTotal():
-        tot = kor + eng + mat
+class SungJukService:
+
+    def getTotal(self, sj):
+        tot = sj.kor + sj.eng + sj.mat
         return tot
 
     def getAverage(self):
-        avg = tot/3
+        avg = self.getTotal(sj)/3
         return avg
 
     def getGrade(self):
-        avg = getAverage()
-        grd = '가'
-        if avg>=90:
-            grd ='수'
-        elif avg>=80:
-            grd ='우'
-        elif avg>=70:
-            grd ='미'
-        elif avg>=60:
-            grd ='양'
+        grdlist = '가가가가가가미양우수수'
+        var = int(self.getAverage()/10) #0~10까지의 수를 만들기 위해 10으로 나눔(배열의 인덱스 추출을 위해서)
+        grd = grdlist[var]              #만들어진 0~10까지의 값이 있는 변수를 배열 인덱스의 값으로 사용
+        return grd
 
 
-sj = SungJuk('혜교',90,80,70)
-sj.getTotal()
+sj = SungJukVO('지현', 98, 74, 69)
+print(sj.name)
+print(sj.kor)
+print(sj.eng)
+print(sj.mat)
 
+sjsrv = SungJukService()
+print(sjsrv.getTotal(sj))
+print('%.1f' % (sjsrv.getAverage()))
+print(sjsrv.getGrade())
